a36bf1f3b8b5c5ee26d36339132508c27a4ab3e1
Shaum <105980825+Astroneural@users.noreply.github.com>
2022-07-10 14:55:02 -0500

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Shaum',
+  subtitle: '',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    name: 'John Doe',
+    name: 'Shaum',
     photo: '/assets/maskable@192.png',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    status: '🍁',
+    bio: 'Hello world!'
   },
   themeColor: '#3D4451'
 }
