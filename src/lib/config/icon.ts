e49af3f955299cdfbec42cc311c0cbbad9cf13e9
Shaum <105980825+Astroneural@users.noreply.github.com>
2022-07-12 13:43:39 -0500

    Update icon.ts

--- src/lib/config/icon.ts
+++ src/lib/config/icon.ts
@@ -1,25 +1,25 @@
 import type { Icon } from '$lib/types/icon'
 import { site } from '$lib/config/site'
 
-// export const favicon: Icon = {
-  // src: site.protocol + site.domain + '/favicon_small.ico',
-  // sizes: '24x24',
-  // type: 'image/icon'
-// } 
+export const favicon: Icon = {
+  src: site.protocol + site.domain + '/favicon_48x42.png',
+  sizes: '48x42',
+  type: 'image/png'
+} 
 
 export const any: { [key: number]: Icon } = {
   180: {
-    src: site.protocol + site.domain + '/assets/any@180.png',
+    src: site.protocol + site.domain + '/assets/icon_180x180.png',
     sizes: '180x180',
     type: 'image/png'
   },
   192: {
-    src: site.protocol + site.domain + '/assets/any@192.png',
+    src: site.protocol + site.domain + '/assets/icon_1992x192.png',
     sizes: '192x192',
     type: 'image/png'
   },
   512: {
-    src: site.protocol + site.domain + '/assets/any@512.png',
+    src: site.protocol + site.domain + '/assets/icon_512x512.png',
     sizes: '512x512',
     type: 'image/png'
   }
