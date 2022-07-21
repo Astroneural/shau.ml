bf2d995e75da027264cec1ce61f9275551890eb5
Shaum <105980825+Astroneural@users.noreply.github.com>
2022-07-20 19:32:26 -0500

    Update icon.ts

--- src/lib/config/icon.ts
+++ src/lib/config/icon.ts
@@ -2,24 +2,24 @@ import type { Icon } from '$lib/types/icon'
 import { site } from '$lib/config/site'
 
 export const favicon: Icon = {
-  src: site.protocol + site.domain + '/favicon_48x42.png',
+  src: 'https://shau.ml/favicon_48x42.png',
   sizes: '48x42',
   type: 'image/png'
 } 
 
 export const any: { [key: number]: Icon } = {
   180: {
-    src: site.protocol + site.domain + '/assets/icon_180x180.png',
+    src: 'https://shau.ml/assets/icon_180x180.png',
     sizes: '180x180',
     type: 'image/png'
   },
   192: {
-    src: site.protocol + site.domain + '/assets/icon_1992x192.png',
+    src: 'https://shau.ml/assets/icon_1992x192.png',
     sizes: '192x192',
     type: 'image/png'
   },
   512: {
-    src: site.protocol + site.domain + '/assets/icon_512x512.png',
+    src: 'https://shau.ml/assets/icon_512x512.png',
     sizes: '512x512',
     type: 'image/png'
   }
@@ -27,12 +27,12 @@ export const any: { [key: number]: Icon } = {
 
 export const maskable: { [key: number]: Icon } = {
   192: {
-    src: site.protocol + site.domain + '/assets/icon_192x192.png',
+    src: 'https://shau.ml/assets/icon_192x192.png',
     sizes: '192x192',
     type: 'image/png'
   },
   512: {
-    src: site.protocol + site.domain + '/assets/icon_512x512.png',
+    src: 'https://shau.ml//assets/icon_512x512.png',
     sizes: '512x512',
     type: 'image/png'
   }
