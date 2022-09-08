c33373a4661f1334cc9aec88e6be92cc0c6ffb70
Shaum <105980825+Astroneural@users.noreply.github.com>
2022-09-08 12:40:55 -0500

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'shaum.ml',
   title: 'Shaum',
   subtitle: '',
   lang: 'en-US',
